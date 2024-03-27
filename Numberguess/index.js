b99a8e2aa707dee3d1fcc649ad1ee0f48fe77517
yug2713 <yugpatel2713@gmail.com>
2024-03-27 13:05:00 +0530

    1st js game

---
+++ Numberguess/index.js
@@ -0,0 +1,26 @@
+function check(event){
+    event.preventDefault();
+    let number = document.getElementById("number").value;
+    let guess = 7;
+    let count = 0;
+    while (count <6){
+        if (number == guess){
+            document.getElementById("span-msg").textContent = "Yes its Right !!!"
+            document.getElementById("span-msg").style.color = "orange";
+            break;
+        }
+        else if (number < guess){
+            document.getElementById("span-msg").textContent = "Its too low !!!"
+            document.getElementById("span-msg").style.color = "red";
+            count++;
+            
+        }
+        else if (number > guess){
+            document.getElementById("span-msg").textContent = "Its too high !!!"
+            document.getElementById("span-msg").style.color = "blue";
+            count++;
+        }
+        
+    }
+    // document.getElementById("span-msg").textContent = "Sorry your chances are over!!!"
+}
